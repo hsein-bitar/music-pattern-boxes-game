8485a0e99fd402c2891ae6e9335d37f24c39db4a
Hsein Bitar <hseinbitar22@gmail.com>
2022-05-19 12:43:26 +0300

    html: added favicon js: working on game logic, winning levels

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-window.onload = () => {
-    console.log('loaded');
-    //TODO
-    disable();
-}
-
 
 // reference html elements
-let user = document.getElementById("user");
+let stats = document.getElementById("stats"); //show after prompt
+let user = document.getElementById("user"); //show after prompt
+let level = document.getElementById("level"); //show after prompt
 let board = document.querySelector('#board');//enable
-let box = document.querySelectorAll('.box'); //enable
-let start = document.getElementById('level'); //enable
+let boxes = document.querySelectorAll('.box'); //enable
+let start = document.getElementById('start'); //enable
 
-
-//initiate level at 1
-let currentLevel = 1;
-let user_clicks = [];
-let pattern = [];
+// getting user nme
+let user_prompt = prompt('Hey, who are you 👀? Enter your name please!');
+let user_name = user_prompt ? user_prompt.split(" ") : "user 007".split(" ");
+user_name = user_name.map(x => (x.charAt(0).toUpperCase() + x.slice(1)));
+user.innerText = user_name.join(' ');
+stats.classList.remove('none');
 
 //TODO ref music files
 // let music = ['green', 'yellow', 'red', 'blue'];
@@ -26,90 +23,99 @@ let music = [
     new Audio("assets/sounds/blue.mp3")];
 
 
+//Game variables
+let currentLevel;
+let user_clicks = [];
+let pattern = [];
 
-let clickHandler = (e) => {
-    e.target.classList.add("glow");
-    setTimeout(() => { e.target.classList.remove("glow") }, 200);
+
+let startGame = (e) => {
+    e.preventDefault();
+    currentLevel = 1;
+    level.innerText = currentLevel;
+    pattern = [Math.floor(Math.random() * 4)]
+    playNote(pattern[pattern.length - 1]);
+    //TODO supposed to disable start button
+    enable();
+}
+
+
+
+start.addEventListener('click', startGame);
+
+
+
+
+//click handlers
+let boxClickHandler = (e) => {
+    playNote(e.target.id);
     if (user_clicks.length < currentLevel) {
         user_clicks.push(e.target.id);
         if (pattern[user_clicks.length - 1] != user_clicks[user_clicks.length - 1]) {
-            gameEnd(0);
+            endGame(0);
         }
     }
     if (user_clicks.length == currentLevel) {
         if (pattern[user_clicks.length - 1] == user_clicks[user_clicks.length - 1]) {
             currentLevel++
-            gameEnd(e.target.id);
+            endGame(1);
         } else {
-            gameEnd(0);
+            endGame(0);
         }
     }
 }
 
 
-// functions
-function enable() {
-    start.removeEventListener('click', e => startLevel(currentLevel));
-    // add onclicks to boxes
-    // checks if so far sequence matches, game over when it does not
-    box.forEach((element) => element.addEventListener("click", clickHandler));
 
-    board.classList.add('enabled');
-    box.forEach(element => element.classList.add('enabled'))
-    start.classList.remove('enabled');
+
+
+
+
+
+// helper functions
+function playNote(note) {
+    // TODO play notes after waiting
+    boxes[note].classList.add('glow');
+    music[note].play()
+    setTimeout(() => {
+        boxes[note].classList.remove('glow');
+    }, 300)
 }
 
+function enable() {
+    start.removeEventListener('click', startGame);
+    start.classList.remove('enabled');
+    start.innerText = '...';
+
+    // enable board interaction
+    boxes.forEach((element) => element.addEventListener("click", boxClickHandler));
+    boxes.forEach(element => element.classList.add('enabled'))
+    board.classList.add('enabled');
+}
 
 function disable() {
-    start.addEventListener('click', e => startLevel(currentLevel));
+    start.addEventListener('click', startGame);
     start.innerText = `Start`
-    //remove event listeners
-    box.forEach(element => element.removeEventListener("click", clickHandler));
-    board.classList.remove('enabled');
-    box.forEach(element => element.classList.remove('enabled'));
     start.classList.add('enabled');
 
+    //remove event listeners
+    boxes.forEach(element => element.removeEventListener("click", boxClickHandler));
+    boxes.forEach(element => element.classList.remove('enabled'));
+    board.classList.remove('enabled');
+
 }
 
-function playPattern(pattern) {
-
-    // TODO play notes after waiting
-    for (i in pattern) {
-        console.log(i);
-    }
-}
-
-function gameEnd(bool) {
+function endGame(bool) {
     if (bool) {
-        console.log('lost', bool);
-        disable();
+        console.log('won', bool);
+        user_clicks = [];
+        currentLevel++;
+        level.innerText = currentLevel;
+        pattern.push(Math.floor(Math.random() * 4));
+        playNote(pattern[pattern.length - 1]);
+        enable();
     } else {
         console.log('lost');
         disable();
     }
 }
-
-function startLevel(level) {
-    start.innerText = `Level ${level}`
-    //disable button
-    pattern = generatePattern(level);
-    playPattern(pattern);
-    enable();
-}
-
-function generatePattern(level) {
-    let pattern = [];
-    for (let i = 0; i < level; i++) {
-        pattern.push(Math.floor(Math.random() * 4));
-    }
-    console.log(pattern);
-    return pattern;
-}
-
-
-let user_prompt = prompt('Hey, who are you 👀? Enter your name please!');
-let user_name = user_prompt ? user_prompt.split(" ") : "user 007".split(" ");
-user_name = user_name.map(x => (x.charAt(0).toUpperCase() + x.slice(1)));
-user.innerText = user_name.join(' ');
-console.log(user_name);
-user.classList.remove('none');
